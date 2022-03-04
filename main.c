3e8ed9f8b9ae6cd212b155816d0adb9087780029
taishi otake <taishi@taishinoMac-mini.local>
2022-03-04 14:04:01 +0900

    update

--- main.c
+++ main.c
@@ -1,33 +1,27 @@
 #include <mlx.h>
+#include <stdio.h>
 
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+int	key_hook(int keycode, t_vars *vars)
 {
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	t_vars *b;
+	b = vars;
+	printf("%d\n", keycode);
+	printf("Hello from key_hook!\n");
+	return (0);
 }
 
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
+	t_vars	vars;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
+	mlx_key_hook(vars.win, key_hook, &vars);
+	mlx_mouse_hook(vars.win, key_hook, &vars);
+	mlx_loop(vars.mlx);
 }
